59ff81012aadf200bc3cb90ed6f3ee0b9ddbc124
hasung98 <75055715+hasung98@users.noreply.github.com>
2021-12-13 11:05:43 +0900

    21.12.13

--- hw9.cpp
+++ hw9.cpp
@@ -1,130 +1,285 @@
-#include <opencv2/opencv.hpp> 
+#include <fstream>
 #include <iostream>
-
+#include "opencv/cv.hpp"
+#include <opencv2/dnn.hpp>
 
 using namespace cv;
 using namespace std;
+using namespace dnn;
 
-int main()
-{   
-    int count = 0; // store drawn rectangles
-    int area; // 찾은 boundrect의 넓이
-    vector<vector<Point> > contours; // store count info
-    vector<Vec4i>hierarchy;
+void morphOps_backSub(Mat &thresh);
 
-    VideoCapture cap; // 영상을 불러오는 객체
-    Mat frame, gray_frame; // 비디오 파일을 구성하는 프레임, 특정 프레임에 대한 gray-scale
-    
-    Ptr<BackgroundSubtractor> bg_model = createBackgroundSubtractorMOG2();
-    Mat image, foregroundMask, backgroundImg, foregroundImg;
+void only_people_deep_grabCut(Mat src, Mat &dst);
+void only_people_backSub(Mat src, Mat mask, Mat &dst);
 
-    CascadeClassifier face_classifier; // face detection을 위한 classifier
-    vector<Rect> faces; // 얼굴 정보 식별에 대한 정보를 저장
-    int i; // 반복문 변수
+void face_detection(Mat src, Mat &dst);
+void only_face(Mat src, Mat &dst);
+
+int main() {
+    Ptr<BackgroundSubtractor> bg_model = createBackgroundSubtractorMOG2(500, 20.0, false);
+    Mat frame, onlyPeople, faceDetection, onlyFace, background, foreground, foregroundMask;
+
+    VideoCapture cap;
     
-    int validate_open; // 영상파일 불러오기 성공여부
-    int key_input; // 키보드 입력값
-    int lastKey = 0; // 가장 최근에 입력한 Key의 값을 업데이트, 초기값은 null을 의미하는 0으로 초기화
-    int f_count = 0; 
-    face_classifier.load("haarcascade_frontalface_alt.xml");
-    validate_open = cap.open("Faces.mp4"); // Faces.mp4 파일을 읽음
-    double fps = cap.get(CAP_PROP_FPS);   // 영상의 출력을 위해 프레임 값을 읽어들임
-    
-    // 예외처리 - 해당 파일이 없거나 파일 로딩을 실패한 경우
-    // -> 프로그램의 실행을 중지시킴
-    if(validate_open == 0) {
-        cout << "Error! : No such file!" << endl;
-        return -1;
+    if (cap.open("Faces.mp4") == 0) {
+        cout << "no such file!" << endl;
+        waitKey(0);
     }
-    
-    // 읽어온 비디오 파일을 출력함
-    while(1){
-        key_input = waitKey(fps); // 사용자로부터 키 입력을 대기
 
-        if(key_input != -1) {
-            lastKey = key_input; 
+    int key = -1;
+    int command = -1;
+
+    while (1) {
+        if(!cap.read(frame)) {
+            cap.set(CAP_PROP_POS_FRAMES, 0);
+            cap.read(frame);
         }
-        cap >> frame; // 프레임을 가져옴
-        if(frame.empty()) break; // loop를 빠져나옴
+        resize(frame, frame, Size(800, 440));
         
-        cvtColor(frame, gray_frame, CV_BGR2GRAY); // 읽어온 frame을 gray-scale로 변경 - 내부적인 연산에 활용하기 위해서
-        // f 입력하면 얼굴 인식 
-        if(lastKey == 'f') {
-            f_count++;
-            putText(frame, format("Key input: %d", lastKey), Point(10, 30), FONT_HERSHEY_SIMPLEX, 1, 4);
-            face_classifier.detectMultiScale(
-                gray_frame,
-                faces, 
-                1.1, 
-                5, 
-                0, 
-                Size(70, 70),
-                Size(100, 100)
-            ); 
-            for (i = 0; i < faces.size(); i++) {
-                Point lb(faces[i].x + faces[i].width, faces[i].y + faces[i].height);
-                Point tr(faces[i].x, faces[i].y);
-                rectangle(frame, lb, tr, Scalar(0, 255, 0), 3, 4, 0);
-            }
-            face_classifier.detectMultiScale(
-                gray_frame,
-                faces,
-                1.1, // 각 frame의 scale을 10% 확대함
-                4,   // 최소 3개 이상의 인식이 나타나야 얼굴로 인식함
-                0,   // not used for a new cascade
-                Size(55, 55), // 인식하는 최소 사이즈
-                Size(60, 60)
-            );
-            // 인식한 얼굴을 식별할 수 있도록 bounding rect를 그려냄
-            for (i = 0; i < faces.size(); i++) {
-                Point lb(faces[i].x + faces[i].width, faces[i].y + faces[i].height);
-                Point tr(faces[i].x, faces[i].y);
-                rectangle(frame, lb, tr, Scalar(100, 255, 255), 3, 4, 0);
-            }
+        if (foregroundMask.empty())
+            foregroundMask.create(frame.size(), frame.type());
+
+        bg_model->apply(frame, foregroundMask);
+        GaussianBlur(foregroundMask, foregroundMask, Size(11, 11), 0);
+        threshold(foregroundMask, foregroundMask, 0, 255, THRESH_BINARY);
+
+        if(key == -1 || command == -1) {
+            imshow("Face", frame);
+        }
+        if(key != -1) {
+            if(command == key)
+                command = -1;
+            else 
+                command = key;
+        }
+
+        if(command == 'B') {
+            only_people_deep_grabCut(frame, onlyPeople);
+            imshow("Face", onlyPeople);
+        }
         
-            face_classifier.detectMultiScale(
-                gray_frame,
-                faces,
-                1.1, // 각 frame의 scale을 10% 확대함
-                4,   // 최소 3개 이상의 인식이 나타나야 얼굴로 인식함
-                0,   // not used for a new cascade
-                Size(35, 35), // 인식하는 최소 사이즈
-                Size(41, 41)  // 인식하는 최대 사이즈
-            );
-            // 인식한 얼굴을 식별할 수 있도록 bounding rect를 그려냄
-            for (i = 0; i < faces.size(); i++) {
-                Point lb(faces[i].x + faces[i].width, faces[i].y + faces[i].height);
-                Point tr(faces[i].x, faces[i].y);
-                rectangle(frame, lb, tr, Scalar(255, 255, 100), 3, 4, 0);
-            }
-        }
-        // b입력 배경제거 
-        else if(lastKey == 66 || lastKey == 98) {
-            while(true){
-                cap >> frame;
-                if (foregroundMask.empty()) 
-                    foregroundMask.create(frame.size(), frame.type());
-                    
-                bg_model->apply(frame, foregroundMask);
-                GaussianBlur(foregroundMask, foregroundMask, Size(11, 11), 3.5, 3.5); 
-                threshold(foregroundMask, foregroundMask, 30, 255, THRESH_BINARY); 
-                foregroundImg = Scalar::all(0);
-                frame.copyTo(foregroundImg, foregroundMask);
-                imshow("Faces", frame);
-                waitKey(33);
-            }
-           
-        }
-        // g입력시 배경 삽입
-        else if(lastKey == 71 || lastKey == 103) {
+        if(command == 'b') {
+            only_people_backSub(frame, foregroundMask, onlyPeople);
             
+            imshow("Face", onlyPeople);
         }
-        // 그 이외에 프로그램 내에서 정의되지 않은 입력이 들어온 경우
-        else {
-            putText(frame, format("Key input: %d", lastKey), Point(10, 30), FONT_HERSHEY_SIMPLEX, 1, Scalar(255), 4);
+
+        if(command == 'f') {
+            face_detection(frame, faceDetection);
+            imshow("Face", faceDetection);
         }
-        // 원본 frame을 window Faces에 출력
-        imshow("Faces", frame);
+        
+        if(command == 'g') {
+            only_face(frame, onlyFace);
+            imshow("Face", onlyFace);
+        }
+        key = waitKey(33);
     }
+
     return 0;
+}
+
+void only_people_deep_grabCut(Mat src, Mat &dst) {
+    Mat people[5];
+
+    String modelConfiguration = "deep/yolov2-tiny.cfg";
+    String modelBinary = "deep/yolov2-tiny.weights";
+    Net net = readNetFromDarknet(modelConfiguration, modelBinary);
+    vector<String> classNamesVec;
+    ifstream classNamesFile("deep/coco.names");
+
+    if (classNamesFile.is_open()) {
+        string className = "";
+        while (std::getline(classNamesFile, className)) classNamesVec.push_back(className);
+    }
+        
+    if (src.channels() == 4) cvtColor(src, src, COLOR_BGRA2BGR);
+    
+    // Convert Mat to batch of images
+    Mat inputBlob = blobFromImage(src, 1 / 255.F, Size(416, 416), Scalar(), true, false);
+    net.setInput(inputBlob, "data"); //set the network input
+    Mat detectionMat = net.forward("detection_out"); //compute output
+    
+    float confidenceThreshold = 0.6;
+    int count_people = 0;
+    for (int i = 0; i < detectionMat.rows; i++) {
+        const int probability_index = 5;
+        const int probability_size = detectionMat.cols - probability_index;
+        float *prob_array_ptr = &detectionMat.at<float>(i, probability_index);
+        size_t objectClass = max_element(prob_array_ptr, prob_array_ptr + probability_size) -
+        prob_array_ptr;
+        // prediction probability of each class
+        float confidence = detectionMat.at<float>(i, (int)objectClass + probability_index);
+        // for drawing labels with name and confidence
+        if (confidence > confidenceThreshold) {
+            float x_center = detectionMat.at<float>(i, 0) * src.cols;
+            float y_center = detectionMat.at<float>(i, 1) * src.rows;
+            float width = detectionMat.at<float>(i, 2) * src.cols;
+            float height = detectionMat.at<float>(i, 3) * src.rows;
+            
+            Point p1(cvRound(x_center - width / 2), cvRound(y_center - height / 2));
+            Point p2(cvRound(x_center + width / 2), cvRound(y_center + height / 2));
+            Rect object(p1, p2);
+            Scalar object_roi_color(0, 255, 0);
+            
+            String className = objectClass < classNamesVec.size() ? classNamesVec[objectClass] : format("unknown(%d)", objectClass);
+            if(className.compare("person?")) count_people++;
+            
+            Mat bgModel, fgModel, result;
+            grabCut(src, result, object, bgModel, fgModel, 10, GC_INIT_WITH_RECT);
+            compare(result, GC_PR_FGD, result, CMP_EQ);
+    
+            people[count_people - 1] = Mat(src.size(), CV_8UC3, Scalar(0, 0, 0));
+            src.copyTo(people[count_people - 1], result);
+        }
+    }
+    dst = Mat(src.size(), CV_8UC3, Scalar(0, 0, 0));
+    
+    for(int i = 0; i < count_people; i++)
+        add(dst, people[i], dst);
+
+    putText(dst, format("how many people: %d", count_people), Point(50, 80), FONT_HERSHEY_SIMPLEX, 1, Scalar(255, 255, 255), 4);
+}
+
+void morphOps_backSub(Mat &thresh) {
+    Mat opening = getStructuringElement(MORPH_RECT, Size(25, 25));
+    Mat closing = getStructuringElement(MORPH_RECT, Size(11, 11));
+    
+    morphologyEx( thresh, thresh, MORPH_OPEN, opening );
+    morphologyEx( thresh, thresh, MORPH_CLOSE, closing );
+
+    medianBlur(thresh, thresh, 11);
+}
+
+void only_people_backSub(Mat src, Mat mask, Mat &dst) {
+    Mat people[5];
+
+    String modelConfiguration = "deep/yolov2-tiny.cfg";
+    String modelBinary = "deep/yolov2-tiny.weights";
+    Net net = readNetFromDarknet(modelConfiguration, modelBinary);
+    vector<String> classNamesVec;
+    ifstream classNamesFile("deep/coco.names");
+
+    if (classNamesFile.is_open()) {
+        string className = "";
+        while (std::getline(classNamesFile, className)) classNamesVec.push_back(className);
+    }
+        
+    if (src.channels() == 4) cvtColor(src, src, COLOR_BGRA2BGR);
+    
+    // Convert Mat to batch of images
+    Mat inputBlob = blobFromImage(src, 1 / 255.F, Size(416, 416), Scalar(), true, false);
+    net.setInput(inputBlob, "data"); //set the network input
+    Mat detectionMat = net.forward("detection_out"); //compute output
+    
+    float confidenceThreshold = 0.6;
+    int count_people = 0;
+    for (int i = 0; i < detectionMat.rows; i++) {
+        const int probability_index = 5;
+        const int probability_size = detectionMat.cols - probability_index;
+        float *prob_array_ptr = &detectionMat.at<float>(i, probability_index);
+        size_t objectClass = max_element(prob_array_ptr, prob_array_ptr + probability_size) -
+        prob_array_ptr;
+        // prediction probability of each class
+        float confidence = detectionMat.at<float>(i, (int)objectClass + probability_index);
+        // for drawing labels with name and confidence
+        if (confidence > confidenceThreshold) {
+            String className = objectClass < classNamesVec.size() ? classNamesVec[objectClass] : format("unknown(%d)", objectClass);
+            if(className.compare("person?")) count_people++;
+        }
+    }
+
+    morphOps_backSub(mask);
+    dst = Scalar::all(0);
+    src.copyTo(dst, mask);
+
+    putText(dst, format("There are %d people", count_people), Point(50, 80), FONT_HERSHEY_SIMPLEX, 1, Scalar(255, 255, 255), 4);
+}
+
+void face_detection(Mat src, Mat &dst) {
+    CascadeClassifier face_classifier;
+    Mat grayframe;
+    vector<Rect> faces;
+    vector<double> face_sizes;
+    int i;
+    
+    face_classifier.load("haarcascade_frontalface_alt.xml");
+    
+    cvtColor(src, grayframe, COLOR_BGR2GRAY);
+    face_classifier.detectMultiScale(
+                grayframe,
+                faces,
+                1.1, // increase search scale by 10% each pass
+                3, // merge groups of three detections
+                0, // not used for a new cascade
+                Size(20, 20), //min size
+                Size(50, 50) //max ize
+    );
+
+    for (i = 0; i < faces.size(); i++) {
+        face_sizes.push_back(faces[i].area());
+    }
+
+    int neareast = max_element(face_sizes.begin(), face_sizes.end()) - face_sizes.begin();
+    int farthest = min_element(face_sizes.begin(), face_sizes.end()) - face_sizes.begin();
+
+    for (i = 0; i < faces.size(); i++) {
+        Point lb(faces[i].x + faces[i].width, faces[i].y + faces[i].height);
+        Point tr(faces[i].x, faces[i].y);
+        if(i == neareast) 
+            rectangle(src, lb, tr, Scalar(0, 255, 0), 3, 4, 0);
+        
+        else if(i == farthest)
+            rectangle(src, lb, tr, Scalar(255, 0, 0), 3, 4, 0);
+
+        else
+            rectangle(src, lb, tr, Scalar(30, 255, 255), 3, 4, 0);
+    }
+    src.copyTo(dst);
+}
+
+void only_face(Mat src, Mat &dst) {
+    CascadeClassifier face_classifier;
+    Mat grayframe;
+    vector<Rect> faces;
+    Mat faceGrabCut[3];
+    int i;
+    
+    face_classifier.load("haarcascade_frontalface_alt.xml");
+    
+    cvtColor(src, grayframe, COLOR_BGR2GRAY);
+    face_classifier.detectMultiScale(
+                grayframe,
+                faces,
+                1.1, // increase search scale by 10% each pass
+                3, // merge groups of three detections
+                0, // not used for a new cascade
+                Size(20, 20), //min size
+                Size(50, 50) //max ize
+    );
+    
+    Mat mask = Mat(src.size(), CV_8UC1, Scalar(0, 0, 0));
+
+    for (i = 0; i < faces.size(); i++) {
+        Point lb(faces[i].x + faces[i].width, faces[i].y + faces[i].height);
+        Point tr(faces[i].x, faces[i].y);
+        Mat bgModel, fgModel, result;
+
+        grabCut(src, result, Rect(lb, tr), bgModel, fgModel, 10, GC_INIT_WITH_RECT);
+        compare(result, GC_PR_FGD, result, CMP_EQ);
+        add(result, mask, mask);
+        
+        faceGrabCut[i] = Mat(src.size(), CV_8UC3, Scalar(0, 0, 0));
+        src.copyTo(faceGrabCut[i], result);
+    }
+    dst = Mat(src.size(), CV_8UC3, Scalar(0, 0, 0));
+    
+    for(int i = 0; i < faces.size(); i++)
+        add(dst, faceGrabCut[i], dst);
+    
+    Mat bg = imread("background.png");
+    resize(bg, bg, Size(800, 440));
+    threshold(mask, mask, 20, 255, THRESH_BINARY_INV);
+
+    bg.copyTo(dst, mask);
 }
